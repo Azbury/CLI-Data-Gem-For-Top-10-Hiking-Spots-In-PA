0b20112c9dc200b4af9a7a132115d752da3ecbef
Austin Asbury <acasbury25@aol.com>
2019-09-14 03:19:03 +0000

    adding class method print_hikingspot_description

--- lib/cli.rb
+++ lib/cli.rb
@@ -7,10 +7,17 @@ class CLI
 
   def self.start
     puts "These are the top 10 Hiking Spots in PA listed in order from number 1 to 10."
+    puts ""
     CLI.print_hikingspot_titles
+    puts ""
+    puts "Type in the title of hiking spot if you would like to see a description of a specific hiking spot."
   end
 
   def self.print_hikingspot_titles
     HikingSpot.all.each {|hikingspot| puts hikingspot.title}
+  end
+
+  def self.print_hikingspot_description (title)
+    HikingSpot.all.detect {|hikingspot| hikingspot.title == title}
   end
 end
