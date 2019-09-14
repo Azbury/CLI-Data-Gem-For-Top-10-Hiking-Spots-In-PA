8d10f55c7c70da2f6867b1af714c16f813383576
Austin Asbury <acasbury25@aol.com>
2019-09-14 00:58:12 +0000

    adding get_page and scrape_spot_titles class methods

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,13 @@
 class Scraper
-  def get_page
+  def self.get_page
+    Nokogiri::HTML(open(https://www.onlyinyourstate.com/pennsylvania/pa-hiking/))
+  end
+
+  def self.scrape_spot_titles
+    spot_title_array = []
+    self.class.get_page.css('.gallery-caption') {|title| spot_title_array << title}
+    spot_title_array
+  end
+
 
 end
