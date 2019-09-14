ea2a8ecd70c116e9da1ba4a316417f4230b6dd12
Austin Asbury <acasbury25@aol.com>
2019-09-14 23:31:33 +0000

    adding comments

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,10 +1,18 @@
+#CLI
+#class for storing all the information needed for the CLI.
+#Call on methods within the HikingSpot class to retrieve information for the CLI.
 class CLI
+
+  #call
+  #class method for making the initial call to the CLI class.
   def self.call
     HikingSpot.new_from_list
     puts "Welcome to the Top 10 Hiking Spots in Pennsalvania."
     CLI.start
   end
 
+  #start
+  #class method for running through the program.
   def self.start
     puts "These are the top 10 Hiking Spots in PA listed in order from number 1 to 10."
     puts ""
@@ -15,12 +23,18 @@ class CLI
     CLI.print_hikingspot_description(input)
   end
 
+  #print_hikingspot_titles
+  #class method for print_hikingspot_titles from hikingspot class.
   def self.print_hikingspot_titles
     HikingSpot.all.each {|hikingspot| puts hikingspot.title}
   end
 
+  #print_hikingspot_description
+  #class method for printing_hikingspot_description based on the number associated with
+  #the title.
   def self.print_hikingspot_description (number)
     hikingspot = HikingSpot.all.detect {|hikingspot| hikingspot.title.include?(number)}
     puts hikingspot.description
   end
+
 end
