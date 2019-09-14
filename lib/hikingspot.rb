99e18a1128ca6d2fa065363d8076e5f23d2cb4f1
Austin Asbury <acasbury25@aol.com>
2019-09-14 02:06:40 +0000

    adding init, self.all, self.new_from_list

--- lib/hikingspot.rb
+++ lib/hikingspot.rb
@@ -1,3 +1,20 @@
 class HikingSpot
+  @@all = []
 
+  attr_accessor :title, :description
+
+  def initialize (title, description)
+    @title = title
+    @description = description
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_from_list
+    titles = Scraper.scrape_spot_titles
+    descriptions = Scraper.scrape_spot_descriptions
+  end
 end
