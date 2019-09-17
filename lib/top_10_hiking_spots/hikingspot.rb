89219279ca4999af5e1a26c894c435936a68f287
Austin Asbury <acasbury25@aol.com>
2019-09-17 14:13:03 +0000

    adding class method find_by_word

--- lib/top_10_hiking_spots/hikingspot.rb
+++ lib/top_10_hiking_spots/hikingspot.rb
@@ -36,4 +36,12 @@ class Top10HikingSpots::HikingSpot
     end
   end
 
+  #find_by_word
+  #Class method for finding an instance of an article based on a word passed
+  #in through a parameter then comparing it to the titles of the individual
+  #articles
+  def self.find_by_word (word)
+    self.all.select {|article| article.title.include?(name)}
+  end
+
 end
